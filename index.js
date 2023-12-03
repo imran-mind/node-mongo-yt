7ddc5663dad6d99e0e00d9e00f5f9a8474ce07d5
imran-mind <shaikhimran115@gmail.com>
2023-12-03 17:09:37 +0530

    Pagination

--- index.js
+++ index.js
@@ -20,9 +20,20 @@ init();
 
 app.get('/movies', async (req,res)=>{
     try{
-        const movies = await db.collection('movies').find({}).sort({name: 1});
+        const page = parseInt(req.query.page); // '1' -> 1
+        const limit = parseInt(req.query.limit);
+        const skipCount = page * limit;
+        const projection = { name: 1, rating:1};
+        const movies = await db.collection('movies').find({})
+            .skip(skipCount)
+            .limit(limit)
+            .sort({name: -1})
+            .project(projection)
+            .toArray();
+        console.log('len ',movies.length);
         res.status(200).json(movies);
     }catch(err){
+        console.log(err);
         res.status(500).json("Internal server error");
     }
 })
@@ -37,3 +48,4 @@ app.get('/movies/:id',async (req,res)=>{
         res.status(500).json({message: "internal server error"});
     }
 })
+
