cafb0ec81497aa4d3043bf8e73a5b9b9a698ef53
imran-mind <shaikhimran115@gmail.com>
2023-12-03 18:20:11 +0530

    updated apis

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const {getMongoDBConnection} = require('./db-connection');
 const {ObjectId} = require('mongodb');
 
 app.use(express.json());
+
 let db;
+let movies;
 async function init(){
     try{
         db = await getMongoDBConnection();
+        movies = db.collection("movies");
         console.log('MongoDB connected...');
         app.listen(8080, ()=>{
             console.log('Server is running on port:8080');
@@ -24,14 +27,13 @@ app.get('/movies', async (req,res)=>{
         const limit = parseInt(req.query.limit);
         const skipCount = page * limit;
         const projection = { name: 1, rating:1};
-        const movies = await db.collection('movies').find({})
+        const data = await movies.find({})
             .skip(skipCount)
             .limit(limit)
             .sort({name: -1})
-            // .project(projection)
+            .project(projection)
             .toArray();
-        console.log('len ',movies.length);
-        res.status(200).json(movies);
+        res.status(200).json(data);
     }catch(err){
         console.log(err);
         res.status(500).json("Internal server error");
@@ -42,7 +44,7 @@ app.get('/movies', async (req,res)=>{
 app.get('/movies/:id',async (req,res)=>{
     try{
         const id = req.params.id;
-        const result = await db.collection('movies').findOne({_id: new ObjectId(id)});
+        const result = await movies.findOne({_id: new ObjectId(id)});
         res.status(200).json(result);
     }catch(err){
         res.status(500).json({message: "internal server error"});
@@ -53,7 +55,7 @@ app.post('/movies',async (req,res)=>{
     try{
         const body = req.body;
         console.log('body ',body);
-        await db.collection('movies').insertOne({...body});
+        await movies.insertOne({...body});
         res.status(201).json({message:"Success!"})
     }catch(err){
         console.log(err);
@@ -68,8 +70,7 @@ app.put('/movies/:id', async (req,res)=>{
         const query = {_id: new ObjectId(req.params.id)};
         const updateDoc = {$set: {...body}};
         const options = { upsert: true };
-        const updated = await db.collection('movies')
-            .updateOne(query, updateDoc, options);
+        const updated = await movies.updateOne(query, updateDoc, options);
         console.log('updated---',updated);
         res.status(200).json({message: 'Updated!'});
     }catch(err){
