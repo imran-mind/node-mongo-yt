554c1b5cffb0935aba503b65bcfd7e8935d14530
imran-mind <shaikhimran115@gmail.com>
2023-12-05 23:24:48 +0530

    sort and projection

--- index.js
+++ index.js
@@ -17,20 +17,19 @@ async function init(){
 }
 init();
 
-
+// asc-> 1
+// desc-> -1
 app.get('/movies', async (req,res)=>{
     try{
-        const page = parseInt(req.query.page); // '1' -> 1
-        const limit = parseInt(req.query.limit);
+        const page = parseInt(req.query.page) || 0;
+        const limit = parseInt(req.query.limit) || 10;
         const skipCount = page * limit;
-        const projection = { name: 1, rating:1};
         const movies = await db.collection('movies').find({})
             .skip(skipCount)
             .limit(limit)
             .sort({name: -1})
-            .project(projection)
+            .project({name: 0, rating: 0})
             .toArray();
-        console.log('len ',movies.length);
         res.status(200).json(movies);
     }catch(err){
         console.log(err);
